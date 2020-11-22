bd1025f8293397ba4c72f3292779919f551e3a41
Daniel Sarno <dansarno91@gmail.com>
2020-11-22 20:30:16 +0000

    Simple radio button and game loop test

--- gamesite/enter/views.py
+++ gamesite/enter/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+games = ["foo", "bar", "baz"]
+
 
 def index(request):
-    return render(request, "enter/index.html")
+    return render(request, "enter/index.html", {
+        "games": games
+    })
