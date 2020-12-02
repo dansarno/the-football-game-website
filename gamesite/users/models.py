e38521f408d769cea3a04c2481bfdce24771511d
Daniel Sarno <dansarno91@gmail.com>
2020-12-02 23:46:34 +0000

    Have signal working when in user models but not in their own signals.py - working but needs more research

--- gamesite/users/models.py
+++ gamesite/users/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -8,3 +11,15 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"Profile: {self.user.username}"
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    print("something")
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
