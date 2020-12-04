83dc370b10840d65467b389ae0f73a5be334bf14
Daniel Sarno <dansarno91@gmail.com>
2020-12-04 22:26:49 +0000

    Added access code functionality to registration, currently just hard coded in but will add ability for admin to generate codes

--- gamesite/users/forms.py
+++ gamesite/users/forms.py
@@ -1,11 +1,19 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.core.validators import RegexValidator
 
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
+    access_code = forms.CharField(max_length=6, validators=[RegexValidator(r'^\d{1,10}$')])
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2', 'access_code']
+
+    def clean_access_code(self, *args, **kwargs):
+        access_code = self.cleaned_data.get("access_code")
+        if not access_code == "123456":
+            raise forms.ValidationError("This is not a valid access code")
+        return access_code
