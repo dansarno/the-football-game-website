85dcdc5684cff06b8d4578ab37a31aced87ce861
Daniel Sarno <dansarno91@gmail.com>
2020-12-05 11:39:03 +0000

    Started to customise the admin to make it easier to use

--- gamesite/enter/admin.py
+++ gamesite/enter/admin.py
@@ -2,12 +2,47 @@ from django.contrib import admin
 from .models import Team, FinalMatch, Group, GroupMatch, \
     Player, Country, Venue, Tournament, GroupMatchBet
 
-admin.site.register(Team)
-admin.site.register(FinalMatch)
-admin.site.register(GroupMatch)
-admin.site.register(Group)
-admin.site.register(Player)
-admin.site.register(Country)
-admin.site.register(Venue)
-admin.site.register(Tournament)
-admin.site.register(GroupMatchBet)
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(FinalMatch)
+class FinalMatchAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(GroupMatch)
+class GroupMatchAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'country_code', 'flag')
+
+
+@admin.register(Venue)
+class VenueAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Tournament)
+class TournamentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(GroupMatchBet)
+class GroupMatchBetAdmin(admin.ModelAdmin):
+    pass
--- gamesite/users/admin.py
+++ gamesite/users/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Profile, AccessCode
 
-admin.site.register(Profile)
-admin.site.register(AccessCode)
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(AccessCode)
+class AccessCodeAdmin(admin.ModelAdmin):
+    list_display = ('code', 'remaining')
