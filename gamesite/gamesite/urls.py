b7c44d3c2df8109f56ca8c46cad3b384bf501fcc
Daniel Sarno <dansarno91@gmail.com>
2020-12-05 00:52:47 +0000

    Now have the feed as the default route with the nav bar links working correctly

--- gamesite/gamesite/urls.py
+++ gamesite/gamesite/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('enter/', include("enter.urls")),
-    path('feed/', include("feed.urls"))
+    path('', include("feed.urls"))
 ]
 
 if settings.DEBUG:
