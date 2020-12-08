c46057df1d45d1b79095368a82bbc798bc004581
Daniel Sarno <dansarno91@gmail.com>
2020-12-08 23:55:05 +0000

    Adding profile urls to users app

--- gamesite/users/urls.py
+++ gamesite/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from . import views
+from .views import profile, profile_edit
 
 app_name = "users"
 urlpatterns = [
-
+    path('profile/', profile, name='profile'),
+    path('profile/edit', profile_edit, name='profile_edit'),
 ]
