d27561612063a9d8823e95b1c0ed37a296040697
Daniel Sarno <dansarno91@gmail.com>
2020-12-20 13:11:05 +0000

    Success with modelchoice fields and custom radio label from object instance

--- gamesite/enter/forms.py
+++ gamesite/enter/forms.py
@@ -2,14 +2,21 @@ from django import forms
 from .models import GroupMatch, GroupMatchOutcome
 
 
-class GroupMatchForm(forms.ModelForm):
-    class Meta:
-        model: GroupMatch
+class NameModelChoiceField(forms.ModelChoiceField):
+    def label_from_instance(self, obj):
+        return f'{obj.winning_amount}'
 
 
 class GroupMatchOutcomeForm(forms.Form):
-    outcomes = forms.ModelChoiceField(queryset=GroupMatchOutcome.objects.none())
-
-    def __init__(self, match):
-        super(GroupMatchOutcomeForm, self).__init__()
-        self.fields['outcomes'].queryset = match.groupmatchoutcome_set.all
+    match1 = NameModelChoiceField(queryset=GroupMatchOutcome.objects.filter(match_id=1).order_by('-result'),
+                                  required=True,
+                                  widget=forms.RadioSelect,
+                                  label=GroupMatch.objects.get(pk=1))
+    match2 = NameModelChoiceField(queryset=GroupMatchOutcome.objects.filter(match_id=2).order_by('-result'),
+                                  required=True,
+                                  widget=forms.RadioSelect,
+                                  label=GroupMatch.objects.get(pk=2))
+    match3 = NameModelChoiceField(queryset=GroupMatchOutcome.objects.filter(match_id=3).order_by('-result'),
+                                  required=True,
+                                  widget=forms.RadioSelect,
+                                  label=GroupMatch.objects.get(pk=3))
--- gamesite/enter/views.py
+++ gamesite/enter/views.py
@@ -2,26 +2,12 @@ from django import forms
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from .forms import GroupMatchForm, GroupMatchOutcomeForm
-from .models import GroupMatch, GroupMatchOutcome
+from .forms import GroupMatchOutcomeForm
 
-games = ["foo", "bar", "baz"]
 submissions = []
 
 
-class GroupGamesForm1(forms.Form):
-    CHOICES = [("home", "Home"), ("draw", "Draw"), ("away", "Away")]
-    game1 = forms.ChoiceField(choices=CHOICES, required=True, widget=forms.RadioSelect, label="Turkey vs Italy")
-    game2 = forms.ChoiceField(choices=CHOICES, required=True, widget=forms.RadioSelect, label="Wales vs Switzerland")
-    game3 = forms.ChoiceField(choices=CHOICES, required=True, widget=forms.RadioSelect, label="England vs Crotia")
-
-
-class GroupGamesForm2(forms.Form):
-    match1 = forms.ModelChoiceField(queryset=GroupMatch.objects.first().groupmatchoutcome_set.all())
-
-
-def index(request, form_class=GroupGamesForm1, template_name="enter/index.html", success_url="enter:confirm"):
-    matches = GroupMatch.objects.all()
+def index(request, form_class=GroupMatchOutcomeForm, template_name="enter/index.html", success_url="enter:confirm"):
     if request.method == "POST":
         form = form_class(request.POST)
         if form.is_valid():
@@ -36,7 +22,6 @@ def index(request, form_class=GroupGamesForm1, template_name="enter/index.html",
 
     return render(request, template_name, {
         "title": "Enter",
-        "matches": matches,
         "form": form_class()
     })
 
