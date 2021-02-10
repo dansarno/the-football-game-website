7d76783a8c754a5e63ad8ec4b3d995596fbfa876
Daniel Sarno <dansarno91@gmail.com>
2021-02-10 23:20:50 +0000

    Bootstrap styling for messages

--- gamesite/gamesite/settings.py
+++ gamesite/gamesite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -143,3 +144,12 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # EMAIL_USE_TLS = ''
 # EMAIL_HOST_USER = ''
 # EMAIL_HOST_PASSWORD = ''
+
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
