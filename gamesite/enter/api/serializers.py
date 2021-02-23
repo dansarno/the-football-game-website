6f1f90308066802d1e886aca2d63af6fc7e41ad7
Daniel Sarno <dansarno91@gmail.com>
2021-02-23 10:01:11 +0000

    Form now showing in table

--- gamesite/enter/api/serializers.py
+++ gamesite/enter/api/serializers.py
@@ -12,6 +12,15 @@ class CalledBetSerializer(serializers.ModelSerializer):
         fields = ['outcome', 'date']
 
 
+class BetSerializer(serializers.ModelSerializer):
+    outcome = serializers.StringRelatedField(read_only=True)
+    called_bet = CalledBetSerializer(read_only=True)
+
+    class Meta:
+        model = models.Bet
+        fields = ['success', 'outcome', 'called_bet']
+
+
 class ScoreLogSerializer(serializers.ModelSerializer):
     called_bet = CalledBetSerializer(read_only=True)
 
@@ -49,10 +58,11 @@ class EntrySerializer(serializers.ModelSerializer):
     profile = ProfileSerializer(read_only=True)
     score_logs = serializers.SerializerMethodField('get_last_five_score_logs')
     position_logs = serializers.SerializerMethodField('get_last_five_position_logs')
+    form = serializers.SerializerMethodField('get_last_five_bets')
 
     class Meta:
         model = models.Entry
-        fields = ['id', 'label', 'profile', 'current_score', 'current_position', 'score_logs', 'position_logs']
+        fields = ['id', 'label', 'profile', 'current_score', 'current_position', 'score_logs', 'position_logs', 'form']
 
     def get_last_five_score_logs(self, obj):
         score_logs = models.ScoreLog.objects.filter(entry=obj).order_by('-called_bet__date')[:5]
@@ -62,4 +72,9 @@ class EntrySerializer(serializers.ModelSerializer):
     def get_last_five_position_logs(self, obj):
         position_logs = models.PositionLog.objects.filter(entry=obj).order_by('-called_bet__date')[:5]
         serializer = PositionLogSerializer(instance=reversed(position_logs), many=True)
+        return serializer.data
+
+    def get_last_five_bets(self, obj):
+        recent_bets = models.Bet.objects.filter(entry=obj).exclude(success__isnull=True).order_by('-called_bet__date')[:5]
+        serializer = BetSerializer(instance=reversed(recent_bets), many=True)
         return serializer.data
---
+++ gamesite/enter/static/enter/overall_leaderboard.js
@@ -0,0 +1,118 @@
+$(document).ready(function() {
+  var tableDataEndpoint = $("#table_container").attr("data-url-endpoint");
+  createTable();
+
+  function createTable() {
+    $('#overall_leaderboard').DataTable({
+      ajax: {
+        "url": tableDataEndpoint,
+        "dataSrc": ""
+      },
+      // processing: true,
+      orderClasses: false,
+      responsive: true,
+      order: [
+        [0, "asc"]
+      ],
+      columns: [{
+          data: "current_position",
+          render: function(data, type, row, meta) {
+            if (type === 'display' || type === 'filter') {
+              let latestPositionLog = row.position_logs[row.position_logs.length - 1];
+              let previousPositionLog = row.position_logs[row.position_logs.length - 2];
+              if (previousPositionLog) {
+                if (latestPositionLog.position > previousPositionLog.position) {
+                  return `<div>${data}</div> <div class="arrow-down"></div>`;
+                } else if (latestPositionLog.position < previousPositionLog.position) {
+                  return `<div class="arrow-up"></div> <div>${data}</div>`;
+                } else {
+                  return data;
+                }
+              } else {
+                return data;
+              }
+            }
+            return data;
+          }
+        },
+        {
+          data: "profile",
+          render: function(data, type, row) {
+            return `<a href="/profile/${data.user.username}" class="pic-link">
+                              <img class="rounded-circle account-img-sm" src="${data.profile_picture}" />
+                            </a>
+                            ${row.label ? row.label : ''}`;
+          }
+        },
+        {
+          data: "profile.user.username",
+          render: function(data, type, row, meta) {
+            if (type === 'display' || type === 'filter') {
+              return `<a href="/profile/${data}" class="text-dark">${data}</a>`;
+            }
+            return data;
+          }
+        },
+        {
+          data: "profile",
+          render: function(data, type, row) {
+            return `${data.user.first_name} ${data.user.last_name}`;
+          }
+        },
+        {
+          data: "form",
+          render: function(data, type, row) {
+            form_arr = '';
+            for (let bet of data) {
+              if (bet.success) {
+                form_arr += `<span data-toggle="tooltip" data-placement="bottom" title="${bet.outcome}">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle correct" viewBox="0 0 16 16">
+                          <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                          <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/>
+                        </svg>
+                      </span>`;
+              } else {
+                form_arr += `<span data-toggle="tooltip" data-placement="bottom" title="${bet.outcome}">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle incorrect" viewBox="0 0 16 16">
+                          <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                          <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                        </svg>
+                      </span>`;
+              }
+            }
+            return form_arr;
+          }
+        },
+        {
+          data: "profile",
+          render: function(data, type, row) {
+            return `${data.user.first_name} ${data.user.last_name}`;
+          }
+        },
+        {
+          data: "current_score",
+          render: $.fn.dataTable.render.number(',')
+        },
+      ],
+      columnDefs: [{
+        targets: '_all',
+        defaultContent: "-"
+      }, {
+        targets: [1, 4, 5],
+        orderable: false
+      }, {
+        targets: 0,
+        className: 'column-bolded'
+      }, {
+        targets: 0,
+        responsivePriority: 1
+      }, {
+        targets: 2,
+        responsivePriority: 3
+      }, {
+        targets: -1,
+        responsivePriority: 2
+      }, ],
+    });
+  }
+});
